73c31ea3e36ed9bc17b9ffdce3203abc120395e6
cen69005 <abelikova@csas.cz>
2024-11-14 14:52:21 +0100

    feat(docs): clean up in unused methods

--- src/main/java/com/example/demoeshop/general/services/product/ProductReviewService.java
+++ src/main/java/com/example/demoeshop/general/services/product/ProductReviewService.java
@@ -2,4 +2,5 @@ package com.example.demoeshop.general.services.product;
 
 public interface ProductReviewService {
     double getAverageRating(Long productId);
+    void addRating(Long productId, double rating);
 }
