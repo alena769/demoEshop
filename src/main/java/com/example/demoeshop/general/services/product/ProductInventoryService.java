d3c071a942ba2786f2df6827492d0a1e1da1da67
cen69005 <abelikova@csas.cz>
2024-11-14 14:51:31 +0100

    feat(docs): clean up in unused methods

--- src/main/java/com/example/demoeshop/general/services/product/ProductInventoryService.java
+++ src/main/java/com/example/demoeshop/general/services/product/ProductInventoryService.java
@@ -2,4 +2,6 @@ package com.example.demoeshop.general.services.product;
 
 public interface ProductInventoryService {
     void adjustStock(Long productId, int quantityChange);
+    int getStockLevel(Long productId);
+    void setStockLevel(Long productId, int newStockLevel);
 }
